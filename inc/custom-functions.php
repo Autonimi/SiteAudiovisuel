f2759c98825f870e44b24fa6d7aeacfc93b1cd90
Aurore <aurore.bordes1@gmail.com>
2018-12-07 15:37:15 +0100

    Page Contact
    Contact ( formulaire(plugin) + Coordonnées de l'entreprise

--- inc/custom-functions.php
+++ inc/custom-functions.php
@@ -3,3 +3,5 @@
 /**
  * Modififer les fonctions ou faire des includes à partir de ce fic hier, ne pas modifier functions.php
  */
+/*include le code qui va créer des widget area ici celle de contact*/ 
+ require get_template_directory() . '/inc/custom-functions-widget-area.php';
