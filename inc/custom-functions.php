b4d552a436560c76a7ad9bc29492f15eae9db17b
Thomas Sarrere <thomas.sarrere@gmail.com>
2018-12-21 23:07:57 +0100

    Script footer

--- inc/custom-functions.php
+++ inc/custom-functions.php
@@ -21,3 +21,6 @@ add_theme_support( 'post-thumbnails' );
 
 //include('ficheProduit-functions-Mikael.php');
 require get_template_directory() . '/inc/ficheProduit-functions-Mikael.php';
+
+wp_enqueue_script( 'script-carousel', get_template_directory_uri() . '/js/carousel.js', array ( 'jquery' ), 1.0, true);
+wp_enqueue_script( 'script-carousel-library', get_template_directory_uri() . '/inc/OwlCarousel2-2.3.4/OwlCarousel2-2.3.4/dist/owl.carousel.min.js', array ( 'jquery' ), 1.0, true);
