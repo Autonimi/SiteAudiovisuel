43075efd4b1d4261d897ca398c28b26dab24e87a
Thomas Sarrere <thomas.sarrere@gmail.com>
2018-12-21 23:11:50 +0100

    Correction jQuery

--- js/carousel.js
+++ js/carousel.js
@@ -1,25 +1,27 @@
-$(document).ready(function(){
-    $("#myBigCarousel").owlCarousel(
-        {
-            
-            dots : false,
-            nav:true,
-            touchDrag  : true,
-            mouseDrag  : true,
-            loop: true,
-            responsiveClass:true,
-            responsive:{
-                0:{
-                    items:3,
-                    nav:false,
-                },
-                600:{
-                    items:5,
-                },
-                1000:{
-                    items:7,
+(function($){
+    $(document).ready(function(){
+        $("#myBigCarousel").owlCarousel(
+            {
+                
+                dots : false,
+                nav:true,
+                touchDrag  : true,
+                mouseDrag  : true,
+                loop: true,
+                responsiveClass:true,
+                responsive:{
+                    0:{
+                        items:3,
+                        nav:false,
+                    },
+                    600:{
+                        items:5,
+                    },
+                    1000:{
+                        items:7,
+                    }
                 }
             }
-        }
-    );
-});
+        );
+    });
+})( jQuery )
